55680fdd46ecf58137453027a10f967e716d36ce
John Kim <cloudiosx@gmail.com>
2022-01-31 10:57:31 -0500

    Disallow answers that are shorter than 3 letters or are just our start word. Refactor all else statements. Add a left bar button item. Fix a subtle bug to make sure the user can't input an uppercase and lowercase version of the same word

--- Project5 - Word Scramble/ViewController.swift
+++ Project5 - Word Scramble/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UITableViewController {
         
         // Create a UIBarButtonItem to run the promptForAnswer() method
         
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startGame))
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
         
         // Find start.txt file URL, convert it to a String instance, then convert it to an array separated by "\n"
@@ -54,7 +55,7 @@ class ViewController: UITableViewController {
         ac.addTextField()
         
         let submitAnswer = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] _ in
-            guard let answer = ac?.textFields?[0].text else { return }
+            guard let answer = ac?.textFields?[0].text?.lowercased() else { return }
             self?.submit(answer)
         }
         
@@ -64,7 +65,7 @@ class ViewController: UITableViewController {
     
     // Methods
     
-    func startGame() {
+    @objc func startGame() {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
@@ -73,37 +74,43 @@ class ViewController: UITableViewController {
     func submit(_ answer: String) {
         let lowercaseAnswer = answer.lowercased()
         
-        let errorTitle: String
-        let errorMessage: String
-        
-        if isPossible(word: lowercaseAnswer) {
-            if isOriginal(word: lowercaseAnswer) {
-                if isReal(word: lowercaseAnswer) {
-                    usedWords.insert(answer, at: 0)
-                    let indexPath = IndexPath(row: 0, section: 0)
-                    tableView.insertRows(at: [indexPath], with: .automatic)
-//                    tableView.reloadData()
-                    return
+        if isSame(word: lowercaseAnswer) {
+            if isPossible(word: lowercaseAnswer) {
+                if isOriginal(word: lowercaseAnswer) {
+                    if isReal(word: lowercaseAnswer) {
+                        usedWords.insert(answer, at: 0)
+                        let indexPath = IndexPath(row: 0, section: 0)
+                        tableView.insertRows(at: [indexPath], with: .automatic)
+    //                    tableView.reloadData()
+                        return
+                    } else {
+                        showErrorMessage(errorTitle: "Word not recognised", errorMessage: "You can't just make them up, you know!")
+                    }
                 } else {
-                    errorTitle = "Word not recognised"
-                    errorMessage = "You can't just make them up, you know!"
+                    showErrorMessage(errorTitle: "Word used already", errorMessage: "Be more original!")
                 }
             } else {
-                errorTitle = "Word used already"
-                errorMessage = "Be more original!"
+                guard let title = title?.lowercased() else { return }
+                showErrorMessage(errorTitle: "Word not possible", errorMessage: "You can't spell that word from \(title)")
             }
         } else {
-            guard let title = title?.lowercased() else { return }
-            errorTitle = "Word not possible"
-            errorMessage = "You can't spell that word from \(title)"
+            showErrorMessage(errorTitle: "Word is same as title", errorMessage: "You can't use the same word as the title!")
         }
-        
+    }
+    
+    func showErrorMessage(errorTitle: String, errorMessage: String) {
         let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
     }
     
     // Conditions to check if the word is possible, original, and real
+    
+    func isSame(word: String) -> Bool {
+        guard let lowercaseTitle = title?.lowercased() else { return false }
+        if (lowercaseTitle == word) { return false }
+        return true
+    }
     
     func isPossible(word: String) -> Bool {
         guard var lowercaseTitle = title?.lowercased() else { return false }
@@ -123,6 +130,7 @@ class ViewController: UITableViewController {
     }
     
     func isReal(word: String) -> Bool {
+        if (word.count < 3) { return false }
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
